3ff5369f13bd5c27bda234cc612b13d6ce7656de
Evtim Kirov <evtimkirov@gmail.com>
2025-09-15 14:58:18 +0300

    Change the file permissions

--- backend/resources/views/app.blade.php
+++ backend/resources/views/app.blade.php
@@ -2,10 +2,16 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Contact Center SPA</title>
-{{--        @vite('resources/js/app.js')--}}
+        <link rel="stylesheet" href="{{ asset('bundle.css') }}">
     </head>
     <body>
         <div id="app"></div>
+        @if(app()->environment('local'))
+            <script src="http://localhost:3001/bundle.js"></script>
+        @else
+            <script src="{{ asset('bundle.js') }}"></script>
+        @endif
     </body>
 </html>
