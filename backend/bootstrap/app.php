661438fe31b96d79941f2b0f0aad4a8f37a7dc71
Evtim Kirov <evtimkirov@gmail.com>
2025-09-15 11:30:26 +0300

    Sanctum part

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -4,6 +4,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\HandleCors;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Illuminate\Cookie\Middleware\EncryptCookies;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,6 +17,11 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->prepend(HandleCors::class);
+        $middleware->api([
+            EncryptCookies::class,
+            StartSession::class,
+            'throttle:60,1',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
