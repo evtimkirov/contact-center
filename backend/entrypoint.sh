547f9179f05d82707e10eca75ae8edc0c8cecff4
Evtim Kirov <evtimkirov@gmail.com>
2025-09-18 13:50:39 +0300

    Add copy env files when the Docker starts

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e
 
+# Copy .env.example to .env
+if [ ! -f .env ]; then
+    cp .env.example .env
+    echo ".env file created from .env.example"
+fi
+
+# Generate the Laravel app key
+php artisan key:generate
+
 echo "Clear the old database data and create new ones"
 php artisan migrate:fresh --seed --force
 
